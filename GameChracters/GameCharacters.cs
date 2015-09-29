64e89608cebceca440ba3c09eef009c4f87d6898
Victoria Teneva <teneva.victoria@gmail.com>
2015-09-29 22:48:12 +0300

    GameCharacter
    Dragon added

---
+++ GameChracters/GameCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class GameCharacters
+{
+
+    static void Main()
+    {
+        Dragon(1, 3);
+    }
+
+    static void Dragon(int x, int y)
+    {
+        Console.SetCursorPosition(x, y);
+        var dragon = new[]
+        {
+            @"        ,       ",
+            @"      _/;	      ",
+            @"  a^-/@ }~(     ",
+            @"~~'\-w / ((   <.",  
+            @"      \ \(/__  )",
+            @"       \( _(_)' ",
+            @"        _> _>	  ",
+        };
+        foreach (var line in dragon)
+        {
+            Console.WriteLine(line);
+        }
+
+    }
+
+}
