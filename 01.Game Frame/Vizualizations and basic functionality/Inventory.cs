9fd2b3593f83ff215f979c5be8b3c4cb8349e009
Georgi Antonov <georgi@hdinteractive.com>
2015-10-02 00:30:30 +0300

    Began work on the Inventory - AddToInventory() method.

--- 01.Game Frame/Vizualizations and basic functionality/Inventory.cs
+++ 01.Game Frame/Vizualizations and basic functionality/Inventory.cs
@@ -1,11 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _01.Game_Frame
 {
-    class Inventory
+    
+    public class Inventory
     {
         // The inventory is an array <string> with the game adding stuff to the inventory at certain times.
+        string[] inventory = new string[8];
+        int drawAtX = 0;
+        int drawAtY = 0;
 
+        int[,] inventoryStartPosition = 
+        {
+            /* x */ {54, 60, 67, 74 },
+            /* y */ {43, 48, 0, 0}
+                                
+        };
+        
+
+        bool inventoryFull = true;
+        string messageToPrintOut = "Can't take item. Inventory full.";
+
+        public string addToInventory(string objName)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (inventory[i] == null)
+                {
+                    inventoryFull = false;
+                    inventory[i] = objName;
+                    switch (i)
+                    {
+                        case 0:
+                            drawAtX = inventoryStartPosition[0, 0];
+                            drawAtY = inventoryStartPosition[1, 0];
+                            break;
+                        case 1:
+                            drawAtX = inventoryStartPosition[0, 1];
+                            drawAtY = inventoryStartPosition[1, 0];
+                            break;
+                        case 2:
+                            drawAtX = inventoryStartPosition[0, 1];
+                            drawAtY = inventoryStartPosition[1, 0];
+                            break;
+                        case 3:
+                            drawAtX = inventoryStartPosition[0, 3];
+                            drawAtY = inventoryStartPosition[1, 0];
+                            break;
+                        case 4:
+                            drawAtX = inventoryStartPosition[0, 4];
+                            drawAtY = inventoryStartPosition[0, 1];
+                            break;
+                        case 5:
+                            drawAtX = inventoryStartPosition[0, 5];
+                            drawAtY = inventoryStartPosition[0, 1];
+                            break;
+                        case 6:
+                            drawAtX = inventoryStartPosition[0, 6];
+                            drawAtY = inventoryStartPosition[0, 1];
+                            break;
+                        case 7:
+                            drawAtX = inventoryStartPosition[0, 7];
+                            drawAtY = inventoryStartPosition[0, 1];
+                            break;
+                    }
+                    break;
+                }
+
+            }
+
+            if (inventoryFull == true)
+            {
+                // MessageBoard(messageToPrintOut, ConsoleColor.Red); //Error with MessageBoard(); not being visible from Inventory.cs
+                return "not added";
+            }
+            else
+            {
+                messageToPrintOut = objName + " has been added to the inventory.";
+                //MessageBoard(messageToPrintOut, ConsoleColor.White);
+                return "added";
+            }
+        }
 
     }
 }
