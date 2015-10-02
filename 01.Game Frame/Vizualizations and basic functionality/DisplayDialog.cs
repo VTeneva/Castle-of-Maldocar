f2ab09d81847ebc184b9075e701e2d0718e2a69c
Victoria Teneva <teneva.victoria@gmail.com>
2015-10-02 12:57:47 +0300

    Dialogs

--- 01.Game Frame/Vizualizations and basic functionality/DisplayDialog.cs
+++ 01.Game Frame/Vizualizations and basic functionality/DisplayDialog.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01.Game_Frame.Vizualizations_and_basic_functionality
+namespace _01.Game_Frame
 {
     class DisplayDialog
     {
+        public void DisplayDialogInDialogBoard()
+        {
+            foreach (var speaker in LevelOneDialogs.levelOneDialogOne())
+            {
+                for (int line = 0; line < LevelOneDialogs[speaker]; line++)
+                {
+
+                }
+            }
+        }
     }
 }
