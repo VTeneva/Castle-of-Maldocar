4de764d97f1c68fc8318d4c580d242c69d9ce7d0
1337m4573r <n4f@abv.bg>
2015-10-08 02:59:52 +0300

    Add class with Global Constants 	new file:   01.Game Frame/GlobalConstants.cs 	modified:   01.Game Frame/01.Game Frame.csproj

---
+++ 01.Game Frame/GlobalConstants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Game_Frame
+{
+    static class GC
+    {
+        //sizes
+        public const int frameThickness = 1;
+        public const int gapThickness = 1;
+        public const int borderTopDownThickness = 3;
+        public const int borderSideThickness = 4;
+        public const int gameFieldLength = 42;
+        public const int gameFieldHeight = 35;
+
+        //coordinates
+        public const int frameLeft = 0; //x
+        public const int frameTop = 4; //y
+        public const int gapLeft = frameLeft + frameThickness; //x
+        public const int gapTop = frameTop + frameThickness; //y
+        public const int borderLeft = gapLeft + gapThickness; //x
+        public const int borderTop = gapTop + gapThickness; //y
+        public const int gameFieldLeft = borderLeft + borderSideThickness; //x
+        public const int gameFieldTop = borderTop + borderTopDownThickness; //y
+        public const int gameFieldRight = gameFieldLeft + gameFieldLength - 1; //x
+        public const int gameFieldBottom = gameFieldTop + gameFieldHeight - 1; //y
+        public const int borderRight = gameFieldRight + borderSideThickness; //x
+        public const int borderBottom = gameFieldBottom + borderTopDownThickness; //y
+        public const int gapRight = borderRight + gapThickness; //x
+        public const int gapBottom = borderBottom + gapThickness; //y
+        public const int frameRight = gapRight + frameThickness; //x
+        public const int frameBottom = gapBottom + frameThickness; //y
+    }
+}
